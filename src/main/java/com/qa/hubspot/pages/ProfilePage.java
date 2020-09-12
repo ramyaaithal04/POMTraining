0a88887910417f004c1d6f3ef0e16e010d849da0
Ramya Aithal <ramyaaithal04@gmail.com>
2020-09-12 14:06:26 +0530

    updated profile page

--- src/main/java/com/qa/hubspot/pages/ProfilePage.java
+++ src/main/java/com/qa/hubspot/pages/ProfilePage.java
@@ -6,6 +6,7 @@ public class ProfilePage {
 	public ProfilePage()
 	{
 		System.out.println("Profile page constructor");
+		System.out.println("To check jenkins automatically executes when we push code to github");
 	}
 
 }
